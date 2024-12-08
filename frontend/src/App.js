dff887b9b0a00ee46f470dd38be05ef276c583e6
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-12-08 10:02:06 +0530

    anu_8_1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,44 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
+
+// function App() {
+//   const [data, setData] = useState(null); // Start with null
+
+//   useEffect(() => {
+//     fetch("/home")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setData(data); // Set the entire response
+//         console.log(data);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       {/* Check if data is null */}
+//       {!data ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <p>{data.message}</p> // Display the "message" property
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const [data, setData] = useState(null); // Start with null
-
-  useEffect(() => {
-    fetch("/home")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data); // Set the entire response
-        console.log(data);
-      });
-  }, []);
-
   return (
-    <div>
-      {/* Check if data is null */}
-      {!data ? (
-        <p>Loading...</p>
-      ) : (
-        <p>{data.message}</p> // Display the "message" property
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
