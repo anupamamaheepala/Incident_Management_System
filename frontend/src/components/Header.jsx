adc661e93f498ae32440a7db69bbf0d6a0b7e68e
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-12-08 16:13:45 +0530

    anu_8_6

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../css/header.css';
+import logo from '../images/SLTlogo.jpg'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <nav className="nav-menu">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/incidents">Incidents</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+      </nav>
+      <div className="auth-buttons">
+        <button className="signin">Sign In</button>
+        <button className="signup">Sign Up</button>
+      </div>
+      
+    </header>
+    
+  );
+};
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
+import '../css/style.css';
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <div>
       <h1>Welcome to the Incident Management System</h1>
       <p>Your one-stop solution for managing and tracking incidents efficiently.</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
+      <p>content</p>
       </div>
       <Footer />
     </div>
