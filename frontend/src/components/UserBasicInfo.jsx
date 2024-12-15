56acc8d01d4f9cff34e4661e9281d7648cf0d7cb
Nethmi De Silva <143713658+NethmiTharushi22@users.noreply.github.com>
2024-12-16 00:18:53 +0530

    create CSS files  and Components

---
+++ frontend/src/components/UserBasicInfo.jsx
@@ -0,0 +1,258 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { Input, Space, message } from "antd";
+
+
+const UserProfileForm = () => {
+    const [user, setUser] = useState({});
+    const [fileList, setFileList] = useState([]);
+
+    useEffect(() => {
+        const fetchUserByID = async () => {
+            const user = JSON.parse(localStorage.getItem("currentUser"));
+            const userID = { userID: user.userID };
+
+            try {
+                const res = await axios.post("/api/users/getUserById", userID);
+                const userData = res.data;
+                console.log(userData);
+
+                // Set user details to state variables
+                setUserId(userData.userID);
+                setFirstName(userData.firstName);
+                setLastName(userData.lastName);
+                setEmail(userData.email);
+                setPhoneNumber(userData.phoneNumber);
+                setUsername(userData.username);
+                setAddress(userData.address1);
+                setCity(userData.city);
+                setProvince(userData.province);
+                setZipCode(userData.zipcode);
+
+                // Set profile picture if available
+                setFileList([
+                    {
+                        uid: "1",
+                        name: "image.png",
+                        status: "done",
+                        url: userData.profilePic,
+                    },
+                ]);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchUserByID();
+    }, []);
+
+    // Add user model use states
+    const [userId, setUserId] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [username, setUsername] = useState("");
+    const [address, setAddress] = useState("");
+    const [city, setCity] = useState("");
+    const [province, setProvince] = useState("");
+    const [zipCode, setZipCode] = useState("");
+
+    const saveChanges = async () => {
+        const updatedUser = {
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            email: email,
+            phoneNumber: phoneNumber,
+            address1: address,
+            city: city,
+            province: province,
+            zipcode: zipCode,
+        };
+
+        try {
+            // const res = await axios.post(`/api/users/updateUser/${userId}`, updatedUser);
+            // console.log("User details updated successfully:", res.data);
+            message.success("User details updated successfully");
+            
+        } catch (error) {
+            console.error("Error updating user details:", error);
+            message.error("Error updating user details");
+        }
+    };
+
+    return (
+        <>
+            <Space direction="vertical">
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        Full Name
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "260px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "260px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </Space>
+
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        User Name
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "260px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </Space>
+
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        NIC
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "360px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Space>
+                <Space direction="horizontal" align="center">   
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        Billing Email
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "360px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                </Space>
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        City
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "360px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                </Space>
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        Contact
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "360px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={province}
+                        onChange={(e) => setProvince(e.target.value)}
+                    />
+                </Space>
+
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        Provice
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "360px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+                </Space>
+
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        Postal Code
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "360px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={zipCode}
+                        onChange={(e) => setZipCode(e.target.value)}
+                    />
+                </Space>
+                <Space direction="horizontal" align="center">
+                    <label style={{ width: "200px", marginTop: "10px" }}>
+                        Address
+                    </label>
+                    <Input
+                        style={{
+                            flex: "auto",
+                            width: "360px",
+                            height: "36px",
+                            flexShrink: 0,
+                            marginTop: "10px",
+                        }}
+                        value={zipCode}
+                        onChange={(e) => setZipCode(e.target.value)}
+                    />
+                </Space>
+            </Space>
+
+            <button className="btn btn-primary save" onClick={saveChanges}
+            style={{margin : "40px", marginLeft : "210px" }}>
+                Save Changes
+            </button>
+        </>
+    );
+};
+
+export default UserProfileForm;
---
+++ frontend/src/components/UserProfile.jsx
@@ -0,0 +1,302 @@
+import React from "react";
+import { Input, Tag } from "antd";
+import Link from "antd/es/typography/Link";
+import profileImg from "../images/profileImg.png";
+import "../css/userProfile.css";
+
+<img src={profileImg} alt="Logo" />;
+
+const UserProfile = () => {
+  return (
+    <div className="container">
+      <div className="bg-image ">
+        <div className="profile-info">
+          <img
+            alt="profileImg"
+            style={{
+              marginTop :"5px",
+              width: "80px",
+              height: "80px",
+            }}
+            src={profileImg}
+          />
+          <div className="btn-container">
+            <Link href="/userProfile/UserSettings">
+                <button className="btn btn-primary edit">Edit Profile</button>
+            </Link>
+          </div>
+          <h2>
+            {"Anuska"} {"Sampath"}
+          </h2>
+          <p>
+            <strong>Email:</strong>
+            <span>
+              <a href="mailto:  [email protected]">{"anuk01@gmail.com"}</a>
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div className="banner_for_update_details">
+        {/* {!allFieldsFilled && ( */}
+        <div className="banner">
+          <Tag className="tag" color="red">
+            Please fill out all required fields. Click
+            <Link href="/userProfile/UserSettings"> here </Link>
+            to update your info.
+          </Tag>
+        </div>
+        {/* )} */}
+      </div>
+
+      <div
+        className="add_user_details_container_right"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "5px",
+          marginBottom: "40px",
+        }}
+      >
+        {/* First Name and Last Name */}
+        <div style={{ display: "flex", marginTop: "8px" }}>
+          <div style={{ marginRight: "8px", flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                First Name
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+
+          <div style={{ flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Last Name
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Username and NIC */}
+        <div style={{ display: "flex", marginTop: "8px" }}>
+          <div style={{ marginRight: "8px", flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Username
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+          <div style={{ flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                NIC
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Billing emial and contact*/}
+        <div style={{ display: "flex", marginTop: "8px" }}>
+          <div style={{ marginRight: "8px", flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Billing Email
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+          <div style={{ flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Contact
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Province and Postal code */}
+        <div style={{ display: "flex", marginTop: "8px" }}>
+          <div style={{ marginRight: "8px", flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Province
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+          <div style={{ flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Postal Code
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+        </div>
+        {/* Address and Status */}
+        <div style={{ display: "flex", marginTop: "8px" }}>
+          <div style={{ marginRight: "8px", flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Address
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+          <div style={{ flex: 1 }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <span
+                style={{
+                  marginBottom: "3px",
+                  fontSize: "12px",
+                }}
+              >
+                Status
+              </span>
+              <Input
+                size="large"
+                style={{ width: "340px", marginRight: "40px" }}
+                value={""}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default UserProfile;
